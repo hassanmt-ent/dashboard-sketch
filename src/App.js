2670507e1e7c0fdb1f938fa83c9ce04cb44c6c9a
Hassan Tariq <hassan@Hassan-MBP-15.local>
2021-01-22 16:59:00 -0600

    changes to the sidebar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Main from './components/Main'
 
-
-
-function App() {
-  return (
-   <div className="wrapper">
-     <Router>
-       <Sidebar/>
-       <Route path='/' component={Main} />
-     </Router>
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Router>
+          <Sidebar />
+          <Route path='/' component={Main} />
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
