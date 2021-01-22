edcc4385bc26041e00d44a02c3b3785d6bc3f392
Hassan Tariq <hassan@Hassan-MBP-15.local>
2021-01-22 16:31:52 -0600

    first

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+import Navbar from './Navbar'
+import Footer from './Footer'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Dashboard from './Dashboard'
+import UserProfile from './UserProfile'
+
+const Main = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Main
